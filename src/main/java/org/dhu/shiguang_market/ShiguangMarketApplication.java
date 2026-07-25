cd2e4d29b3d7cfd2eb921fcdc9b15b856e6e3254
haoran.tu <1642804440@qq.com>
2026-07-25 13:59:09 +0800

    feat:first commit

---
+++ src/main/java/org/dhu/shiguang_market/ShiguangMarketApplication.java
@@ -0,0 +1,13 @@
+package org.dhu.shiguang_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiguangMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiguangMarketApplication.class, args);
+    }
+
+}
